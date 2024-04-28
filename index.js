c37ca76d6bb9e10dfb0a30bc3f2a5f0b5acc1cf1
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-04-28 17:02:50 -0500

    Refactor index.js to enable CORS, JSON parsing, and add routes for books

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express(); // Create an express app
+
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Enable JSON parsing
+
+app.get("/", (req, res) => {
+  res.status(200).json({});
+});
+
+const routesBooks = require("./routes/books");
+app.use("/books", routesBooks);
+
+//MORE ROOUTES
+
+mongoose.connect(
+  "mongodb+srv://cristiancubillos0654:QolYJR7ZvwcCLyL6@cluster0.m7ju5gz.mongodb.net/"
+);
+
+app.listen(8080);
