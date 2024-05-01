31c9c2de2593dcf886080023d8fb24fcd72d0b91
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-01 12:10:42 -0500

    Refactor book.route.js to add GET and POST routes for books, and update HTTP status code to 201

--- book/book.route.js
+++ book/book.route.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { createBook } = require("./book.controller");
+const { createBook, readBookWithFilters } = require("./book.controller");
 
 const { respondWithError } = require("../utils/functions");
 
-function GetBooks(req, res) {
-  res.status(200).json({
-    mensaje: "GET 📚",
-  });
+async function GetBooks(req, res) {
+  try {
+    const resultadosBusqueda = await readBookWithFilters(req.query);
+
+    res.status(200).json({ ...resultadosBusqueda });
+  } catch (e) {
+    respondWithError(res, e);
+  }
 }
 
 async function PostBook(req, res) {
   try {
     await createBook(req.body);
 
-    res.status(201).json({
-      mensaje: "Creado. 🎉",
-    });
+    res.status(201).json(
+      { ...req.body } // Devuelve el libro creado,
+    );
   } catch (e) {
     respondWithError(res, e);
   }
