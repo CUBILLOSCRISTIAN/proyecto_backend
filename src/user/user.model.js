40e83cb5169df9fe37fcd2c7838a4a55488a3d6b
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-01 23:30:02 -0500

    Add auth folder and modify model user

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const schemaUsuario = new mongoose.Schema(
   {
-    nombre: { type: String, required: true },
+    nombre: { type: String, required: true, unique: true },
     email: { type: String, required: true },
     contraseña: { type: String, required: true },
     libros_vendidos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Libro" }],
@@ -14,7 +14,6 @@ const schemaUsuario = new mongoose.Schema(
   }
 );
 
-
 const Usuario = mongoose.model("Usuario", schemaUsuario);
 
-module.exports = {Usuario};
+module.exports = { Usuario };
