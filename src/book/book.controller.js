1c30c66cd3bab0de5f1bf9a164795a2f65b83d3e
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-08 18:43:15 -0500

    refactor: Remove unnecessary code in book.controller.js

--- src/book/book.controller.js
+++ src/book/book.controller.js
@@ -1,8 +1,6 @@
-const { throwCustomError } = require("../../utils/functions");
 const { createBookMongo, getBooksMongo } = require("./book.actions");
 
 async function createBook(data) {
-  //Si necesitamos hacer validaciones, las hacemos aquí, destructurando el objeto data
   const CreatedBook = await createBookMongo(data);
   return CreatedBook;
 }
