50addd3b100c4e62d9ad80cf1a206b87a97435d5
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-08 14:06:38 -0500

    refactor: Add register and login routes to auth.route.js

--- src/auth/auth.route.js
+++ src/auth/auth.route.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const { register, login } = require("./auth.controller");
 
-router.post("/register");
-router.post("/login");
+async function registerUser(req, res) {
+  try {
+    const newUser = await register(req.body);
+    res.status(201).json({ ...newUser });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+}
+
+async function loginUser(req, res) {
+  try {
+    const user = await login(req.body);
+    res.status(200).json(user);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ error: e.message });
+  }
+}
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
