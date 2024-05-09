c7e6ad7a049f8ce13cafae4c231d2ad74371033a
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-09 08:51:07 -0500

    refactor: Update GetUser function in user.route.js

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { respondWithError } = require("../../utils/functions");
+const { GetUserById } = require("./user.controller");
 
 async function GetUser(req, res) {
   try {
-    const user = await readUserById(req.params.id);
+    const user = await GetUserById(req.params.id);
     res.status(200).json(user);
   } catch (e) {
     respondWithError(res, e);
