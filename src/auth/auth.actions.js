70912d57a4c44f9d0b6fdb8d7029f793dd5f8955
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-08 19:46:57 -0500

    refactor: Update token verification in auth.actions.js

--- src/auth/auth.actions.js
+++ src/auth/auth.actions.js
@@ -49,9 +49,10 @@ async function verifyToken(req, res, next) {
 
     if (!token) return res.status(403).json({ message: "No token provided" });
 
-    const decoded = await jwt.verify(token, config.SECRET);
+    const decoded = await jwt.verify(token, config.SECRET)
+    req.userId = decoded.id;;
 
-    const user = await User.findById(decoded.id, { password: 0 });
+    const user = await User.findById(req.userId, { password: 0 });
     if (!user) return res.status(404).json({ message: "No user found" });
     next();
   } catch (error) {
