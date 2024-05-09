037cafc608a3c6e15e64060fa37c1d210ef998b4
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-09 15:47:21 -0500

    refactor: Update order.controller.js to include getSalesman function and remove unnecessary code

--- src/order/order.controller.js
+++ src/order/order.controller.js
@@ -1,11 +1,9 @@
 const { verify } = require("jsonwebtoken");
 const {
   createOrderMongo,
-  getOrdersMongo,
-  getOrderMongoById,
-  updateOrderMongo,
   verifyOnlySalesman,
   getBooksTotalPrice,
+  getSalesman,
 } = require("./order.actions");
 
 async function createOrder(data) {
@@ -14,30 +12,14 @@ async function createOrder(data) {
   if (!verifyOnlySalesman(libros_ids))
     return { error: "Todos los libros deben ser del mismo vendedor." };
 
+  data.vendedor = await getSalesman(libros_ids);
+
   data.total = await getBooksTotalPrice(libros_ids);
 
   const createdOrder = await createOrderMongo(data);
   return createdOrder;
 }
 
-async function readOrdersWithFilters(filters) {
-  const orders = await getOrdersMongo(filters);
-  return orders;
-}
-
-async function readOrderById(id) {
-  const order = await getOrderMongoById(id);
-  return order;
-}
-
-async function updateOrder(id, data) {
-  const updatedOrder = await updateOrderMongo(id, data);
-  return updatedOrder;
-}
-
 module.exports = {
   createOrder,
-  readOrdersWithFilters,
-  readOrderById,
-  updateOrder,
 };
