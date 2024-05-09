5177b3bcb883e06be7ba6908333715ecf0c4eb5b
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-09 15:47:36 -0500

    refactor: Update order.route.js to use async/await syntax for route handlers

--- src/order/order.route.js
+++ src/order/order.route.js
@@ -9,28 +9,9 @@ const {
   deleteOrder,
 } = require("./order.controller");
 
-const { respondWithError } = require("../../utils/functions");
+const { respondWithError, throwCustomError } = require("../../utils/functions");
 
 const { verifyToken } = require("../auth/auth.actions");
-
-//Funciones del controlador
-async function GetOrders(req, res) {
-  try {
-    const orders = await readOrdersWithFilters(req.query);
-    res.status(200).json({ orders });
-  } catch (e) {
-    respondWithError(res, e);
-  }
-}
-
-async function GetOrder(req, res) {
-  try {
-    const order = await readOrderById(req.params.id);
-    res.status(200).json(order);
-  } catch (e) {
-    respondWithError(res, e);
-  }
-}
 
 async function PostOrder(req, res) {
   try {
@@ -38,22 +19,10 @@ async function PostOrder(req, res) {
     const createdOrder = await createOrder(req.body);
     res.status(201).json(createdOrder);
   } catch (e) {
-    respondWithError(res, e);
+    throwCustomError(e);
   }
 }
 
-async function PutOrder(req, res) {
-  try {
-    const updatedOrder = await updateOrder(req.params.id, req.body);
-    res.status(200).json(updatedOrder);
-  } catch (e) {
-    respondWithError(res, e);
-  }
-}
-
-router.get("/", verifyToken, GetOrders); //Obtener todas las ordenes
-router.get("/:id", verifyToken, GetOrder); //Obtener una orden
+//Obtener una orden
 router.post("/", verifyToken, PostOrder); //Crear una orden
-router.put("/:id", verifyToken, PutOrder); //Actualizar una orden
-
 module.exports = router;
