e5589cd533f6be4db0da90e870e94d77ce10f670
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-09 19:21:37 -0500

    refactor: Update order.route.js to include GET endpoint for retrieving an order

--- src/order/order.route.js
+++ src/order/order.route.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  createOrder,
-  readOrdersWithFilters,
-  readOrderById,
-  updateOrder,
-  deleteOrder,
-} = require("./order.controller");
+const { createOrder, getOrder } = require("./order.controller");
 
 const { respondWithError, throwCustomError } = require("../../utils/functions");
 
@@ -19,10 +13,20 @@ async function PostOrder(req, res) {
     const createdOrder = await createOrder(req.body);
     res.status(201).json(createdOrder);
   } catch (e) {
-    throwCustomError(e);
+    respondWithError(res, e);
+  }
+}
+
+async function GetOrder(req, res) {
+  try {
+    const order = await getOrder(req.params.id, req.userId);
+    res.status(200).json(order);
+  } catch (e) {
+    respondWithError(res, e);
   }
 }
 
 //Obtener una orden
 router.post("/", verifyToken, PostOrder); //Crear una orden
+router.get("/:id", verifyToken, GetOrder); //Obtener una orden
 module.exports = router;
