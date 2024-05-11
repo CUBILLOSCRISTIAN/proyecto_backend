7d01423ad7b4839875af09bbfe64c6cc2cd176ec
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-11 10:44:15 -0500

    refactor: Update auth.controller.js to encrypt user password during registration

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,6 +1,11 @@
-const { registerUser, loginUser } = require("./auth.actions");
+const { registerUser, loginUser, encryptPassword } = require("./auth.actions");
 
 async function register(data) {
+  const { name, email, password } = data;
+  if (!name || !email || !password) {
+    throw { status: 400, message: "Missing data" };
+  }
+  data.password = await encryptPassword(password);
   const registeredUser = await registerUser(data);
   return registeredUser;
 }
