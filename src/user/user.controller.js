c91853c36f1792b2d92e3a8b446cb2325d67033f
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-11 10:44:29 -0500

    refactor: Add updateUser function to user.controller.js

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,8 +1,25 @@
-const { GetUserByIdMongo} = require("./user.actions");
+const { GetUserByIdMongo, userUpdateMongo } = require("./user.actions");
+const auth = require("../auth/auth.actions");
 
 async function GetUserById(id) {
   const user = await GetUserByIdMongo(id);
   return user;
 }
 
-module.exports = { GetUserById };
+async function updateUser(id, data) {
+  const { password } = data;
+  if (password) {
+    data.password = await auth.encryptPassword(password);
+  }
+  const userUpdated = await userUpdateMongo(id, data);
+  
+  const updatedUserInfo = {
+    id: userUpdated.id,
+    email: userUpdated.email,
+    name: userUpdated.name,
+  };
+
+  return updatedUserInfo;
+}
+
+module.exports = { GetUserById, updateUser };
