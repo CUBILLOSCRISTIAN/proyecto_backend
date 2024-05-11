1ab50ff84089a0e8ada44411826aa0148dda7d7a
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-11 10:44:37 -0500

    refactor: Add PATCH endpoint for updating user information

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { respondWithError } = require("../../utils/functions");
-const { GetUserById } = require("./user.controller");
+const { GetUserById, updateUser } = require("./user.controller");
 const { verifyToken } = require("../auth/auth.actions");
 
 async function GetUser(req, res) {
@@ -20,6 +20,26 @@ async function GetUser(req, res) {
   }
 }
 
+async function UpdateUser(req, res) {
+  try {
+    if (req.userId != req.params.id) {
+      return respondWithError(res, {
+        status: 403,
+        message: "No tienes permisos para realizar esta acción.",
+      });
+    }
+
+    const user = await updateUser(req.params.id, req.body);
+
+    res
+      .status(200)
+      .json({ message: "Usuario actualizado correctamente.", user: user });
+  } catch (e) {
+    respondWithError(res, e);
+  }
+}
+
 router.get("/:id", verifyToken, GetUser); //Obtener un usuario
+router.patch("/:id", verifyToken, UpdateUser); //Actualizar un usuario
 
 module.exports = router;
